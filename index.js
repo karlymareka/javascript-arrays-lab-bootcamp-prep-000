2aca1c7c411af591a88720e6962e48fddd039fde
karly.mareka@gmail.com <karly.mareka@gmail.com>
2018-05-21 17:30:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 const app = "I don't do much."
+
+var kittens = ['Milo','Otis','Garfield'];
+
+function destructivelyAppendKitten(name) {
+  return kittens.push(name);
+}
+
+function destructivelyPrependKitten(name) {
+  return kittens.unshift(name);
+}
+
+function destructivelyRemoveLastKitten() {
+  return kittens.pop();
+}
+
+function destructivelyRemoveFirstKitten() {
+  return kittens.shift();
+}
+
+function appendKitten(name) {
+  return kittens.concat(name);
+}
+
+function prependKitten(name) {
+  return [name].concat(kittens);
+}
+
+function removeLastKitten() {
+  return kittens.slice(0, 4)
+}  
+  
+  
+  
+  
+}
+ 
